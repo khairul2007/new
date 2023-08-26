1d5db0c8f803de9cbece003576d4a1fd64f2afc4
khairul2007 <khairul.muktagacha@gmail.com>
2023-08-26 16:24:22 +0600

    js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function myFunction() {
   document.getElementById("dem").innerHTML = "paragraph change";
 }
+document.getElementById("sum").innerHTML = 5 + 6;
+document.write(6 + 8);
+alert(10 + 12);
+console.log(12 - 5);
